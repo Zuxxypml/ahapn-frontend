d4477e1786794ad0660d1b45b1185f6097f2e751
appuser <priscillasweetz952@gmail.com>
2025-04-05 10:40:21 +0200

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteTsconfigPaths()],
+  plugins: [react(), tailwindcss(),],
   base: "/edo2025",  // Ensures assets are served under /edo2025
   build: {
     outDir: "build",  // Matches your deployment setup
